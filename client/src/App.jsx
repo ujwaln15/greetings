75e6c706e5bd71eca1f9486159ef5dd3e20635b4
ujwaln15 <ujn1504@gmail.com>
2024-08-19 18:56:39 +0530

    vite + tailwind + react-router setup, views + routes init

---
+++ client/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+import Auth from "./views/auth";
+import Chat from "./views/chat";
+import User from "./views/user";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/user" element={<User />} />
+        <Route path="*" element={<Navigate to="/auth" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
