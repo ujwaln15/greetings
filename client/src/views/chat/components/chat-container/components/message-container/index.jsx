70dcb4768af654a9af6820b8f8822d45237edbed
ujwaln15 <ujn1504@gmail.com>
2024-10-06 21:59:59 +0530

    image display in window

--- client/src/views/chat/components/chat-container/components/message-container/index.jsx
+++ client/src/views/chat/components/chat-container/components/message-container/index.jsx
@@ -1,9 +1,12 @@
 import { apiClient } from "@/lib/api-client";
 import { getTheme } from "@/lib/utils";
 import { useAppStore } from "@/store";
-import { GET_ALL_MESSAGES_ROUTE } from "@/utils/constants";
+import { GET_ALL_MESSAGES_ROUTE, HOST } from "@/utils/constants";
 import moment from "moment";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
+import { MdFilePresent } from "react-icons/md";
+import { IoMdArrowRoundDown } from "react-icons/io";
+import { IoCloseSharp } from "react-icons/io5";
 
 function MessageContainer() {
   const scrollRef = useRef();
@@ -14,6 +17,9 @@ function MessageContainer() {
     selectedChatMessages,
     setSelectedChatMessages,
   } = useAppStore();
+
+  const [showImage, setShowImage] = useState(false);
+  const [imageURL, setImageURL] = useState(null);
 
   useEffect(() => {
     const getMessages = async () => {
@@ -43,6 +49,12 @@ function MessageContainer() {
     }
   }, [selectedChatMessages]);
 
+  const checkIfImage = (filePath) => {
+    const imageRX =
+      /\.(jpg|jpeg|png|gif|bmp|tiff|tif|webp|svg|ico|heic|heif)$/i;
+    return imageRX.test(filePath);
+  };
+
   const renderMessages = () => {
     let lastDate = null;
     return selectedChatMessages.map((message) => {
@@ -60,6 +72,20 @@ function MessageContainer() {
         </div>
       );
     });
+  };
+
+  const downloadFile = async (url) => {
+    const response = await apiClient.get(`${HOST}/${url}`, {
+      responseType: "blob",
+    });
+    const urlBlob = window.URL.createObjectURL(new Blob([response.data]));
+    const link = document.createElement("a");
+    link.href = urlBlob;
+    link.setAttribute("download", url.split("/").pop());
+    document.body.appendChild(link);
+    link.click();
+    link.remove();
+    window.URL.revokeObjectURL(urlBlob);
   };
 
   const renderDuoMessages = (message) => {
@@ -80,6 +106,46 @@ function MessageContainer() {
             {message.content}
           </div>
         )}
+        {message.messageType === "file" && (
+          <div
+            className={`${
+              message.sender !== selectedChatData._id
+                ? getTheme(userInfo.theme)
+                : "border-white/20"
+            } opacity-80 text-opacity-100 border-[4px] inline-block p-4 rounded my-1 max-w-[50%] break-words`}
+          >
+            {checkIfImage(message.fileUrl) ? (
+              <div
+                className="cursor-pointer"
+                onClick={() => {
+                  setShowImage(true);
+                  setImageURL(message.fileUrl);
+                }}
+              >
+                <img
+                  src={`${HOST}/${message.fileUrl}`}
+                  height={300}
+                  width={300}
+                />
+              </div>
+            ) : (
+              <div className="flex items-center justify-center gap-4">
+                <span className="text-white/80 text-3xl bg-black/20 rounded-full p-3">
+                  <MdFilePresent />
+                </span>
+                <span className="text-white">
+                  {message.fileUrl.split("/").pop()}
+                </span>
+                <span
+                  className="bg-black/20 p-3 text-2xl rounded-full hover:bg-black/50 cursor-pointer transition-all duration-300"
+                  onClick={() => downloadFile(message.fileUrl)}
+                >
+                  <IoMdArrowRoundDown />
+                </span>
+              </div>
+            )}
+          </div>
+        )}
         <div className="text-xs text-gray-500">
           {moment(message.timestamp).format("LT")}
         </div>
@@ -91,6 +157,33 @@ function MessageContainer() {
     <div className="flex-1 overflow-y-auto scrollbar-hidden p-4 px-8 md:w-[65vw] lg:w-[70vw] xl:w-[80w] w-full">
       {renderMessages()}
       <div ref={scrollRef} />
+      {showImage && (
+        <div className="fixed z-[1000] top-0 left-0 h-[100vh] w-[100vw] flex items-center justify-center backdrop-blur-lg flex-col">
+          <div>
+            <img
+              src={`${HOST}/${imageURL}`}
+              className="h-[80vh] w-full bg-cover"
+            />
+          </div>
+          <div className="flex gap-5 fixed top-0 mt-5">
+            <button
+              className="bg-black/20 p-3 text-2xl rounded-full hover:bg-black/50 cursor-pointer transition-all duration-300"
+              onClick={() => downloadFile(imageURL)}
+            >
+              <IoMdArrowRoundDown />
+            </button>
+            <button
+              className="bg-black/20 p-3 text-2xl rounded-full hover:bg-black/50 cursor-pointer transition-all duration-300"
+              onClick={() => {
+                setShowImage(false);
+                setImageURL(null);
+              }}
+            >
+              <IoCloseSharp />
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
