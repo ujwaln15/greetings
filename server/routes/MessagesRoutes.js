290105d2ccf99cd3a1ea4b32ecf067438c8015e2
ujwaln15 <ujwal_nayak@hotmail.com>
2025-06-21 13:53:56 +0000

    multer dir update

--- server/routes/MessagesRoutes.js
+++ server/routes/MessagesRoutes.js
@@ -4,7 +4,7 @@ import { getMessages, uploadFile } from "../controllers/MessagesController.js";
 import multer from "multer";
 
 const messagesRoutes = Router();
-const upload = multer({ dest: "uploads/files" });
+const upload = multer({ dest: "/tmp/uploads/files" });
 messagesRoutes.post("/get-messages", verifyToken, getMessages);
 messagesRoutes.post(
   "/upload-file",
